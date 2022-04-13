cedcfb0bd60edcc7ded599ca62db10e1290119be
akamine <22akamine_shun@axiz.co.jp>
2022-04-13 19:46:01 +0900

    発展問題が9割できました。

---
+++ db_exam_dev1.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE axiz_exam_dev;
+
+CREATE table customer (
+customer_id INT PRIMARY KEY
+,customer_name VARCHAR(50)
+);
+
+-- 確認用
+select * from customer; 
+select * from sales;
+
+CREATE table sales (
+sales_id INT PRIMARY KEY
+,order_date DATE
+,customer_id INT NOT NULL
+, FOREIGN KEY(customer_id) REFERENCES customer(customer_id)
+,amount DECIMAL
+);
+
+INSERT into customer
+(customer_id, customer_name) VALUES(1, '田中')
+,(2, '鈴木')
+,(3, '田中')
+,(4, '田島');
+
+INSERT INTO sales
+(sales_id, order_date, customer_id, amount) VALUES(1, '2018/10/01', 1, 3000)
+,(2, '2018/10/01', 3, 5000)
+,(3, '2018/10/01', 4, 6000)
+,(4, '2018/11/02', 2, 2000)
+,(5, '2018/11/15', 2, null);
+
+SELECT * FROM sales 
+WHERE amount = 5000;
+
+SELECT * FROM sales 
+WHERE amount >= 5000;
+
+SELECT sales_id, order_date, customer_id, amount * 1.1 FROM sales ;
+
+SELECT * FROM sales WHERE amount = NOT NULL; -- できない
+
+SELECT * FROM customer WHERE customer_name != '田中';
+
+UPDATE sales SET order_date='2018/11/05', customer_id=4 WHERE sales_id = 4;
+
+DELETE FROM sales WHERE amount = NULL;
+
