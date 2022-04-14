87cc62330f905ebe3fd2361c5d070ef0a2442956
akamine <22akamine_shun@axiz.co.jp>
2022-04-14 18:40:41 +0900

    基本問題の8番を修正しました・

--- enshu2_8.sql
+++ enshu2_8.sql
@@ -1,5 +1,7 @@
 -- 8
-SELECT MIN(order_date), sum(amount) AS sum_amount 
-FROM sales 
-WHERE order_date = (SELECT MIN(order_date) 
-                        FROM sales);
+SELECT student_id, student_name, major_id
+FROM student 
+WHERE major_id IN (SELECT major_id FROM student
+                    GROUP BY major_id
+                    HAVING count(*) >= 3)
+ORDER BY major_id ASC, student_id ASC;
